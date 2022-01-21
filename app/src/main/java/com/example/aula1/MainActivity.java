2fc5e5de1088c27fb86c78721d1a5dc1fe6543cb
carlossevero01 <carlos212427@gmail.com>
2022-01-20 21:59:47 -0300

    Initial commit

---
+++ app/src/main/java/com/example/aula1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.aula1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView ts = (TextView) findViewById(R.id.texto1);
+    }
+}
